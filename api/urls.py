9fed76b0bd5e4e3d5d7b806d32b19c9820cd377f
“krv006” <rvkamronbek@gmail.com>
2024-03-02 23:27:31 +0500

    learn_api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import AuthorListAPIView, BookListAPIView, PublisherListAPIView, MagazineListAPIView
+from .views import AuthorDetailAPIView, AuthorListAPIView, BookListAPIView, PublisherListAPIView, MagazineListAPIView
 
 urlpatterns = [
+
     path('authors/', AuthorListAPIView.as_view(),name='authors-list'),
+    path('authors/<int:pk>/', AuthorDetailAPIView.as_view(),name='authors-detail'),
+
     path('books/', BookListAPIView.as_view(),name='books-list'),
-    path('publishers/', PublisherListAPIView.as_view(),name='authors-list'),
-    path('magazines/', MagazineListAPIView.as_view(),name='authors-list')
+    path('books/<int:pk>/', BookListAPIView.as_view(),name='books-detail'),
+
+    path('publishers/', PublisherListAPIView.as_view(),name='publishers-list'),
+    path('publishers/<int:pk>/', PublisherListAPIView.as_view(),name='publishers-detail'),
+
+    path('magazines/', MagazineListAPIView.as_view(),name='magazines-list'),
+    path('magazines/<int:pk>/', MagazineListAPIView.as_view(),name='magazines-detail'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,37 +1,49 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 from .models import Author, Book, Publisher, Magazine
 from .serializers import AuthorSerializer, BookSerializer, PublisherSerializer, MagazineSerializer
-# BookSerializer
 
 
-class AuthorListAPIView(APIView):
-    def get(self,request):
-        authors = Author.objects.all()
-        serializer = AuthorSerializer(authors, many=True)
-        return Response(serializer.data)
+class AuthorListAPIView(ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class AuthorDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
 
-class BookListAPIView(APIView):
-    def get(self,request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-    
+class BookListAPIView(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
-class PublisherListAPIView(APIView):
-    def get(self,request):
-        publishers = Publisher.objects.all()
-        serializer = PublisherSerializer(publishers, many=True)
-        return Response(serializer.data)
+    # def get(self,request):
+    #     books = Book.objects.all()
+    #     serializer = BookSerializer(books, many=True)
+    #     return Response(serializer.data)
 
 
-class MagazineListAPIView(APIView):
-    def get(self,request):
-        magazines = Magazine.objects.all()
-        serializer = MagazineSerializer(magazines, many=True)
-        return Response(serializer.data)
+class PublisherListAPIView(ListCreateAPIView):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
 
+
+    # def get(self,request):
+    #     publishers = Publisher.objects.all()
+    #     serializer = PublisherSerializer(publishers, many=True)
+    #     return Response(serializer.data)
+
+
+class MagazineListAPIView(ListCreateAPIView):
+    queryset = Magazine.objects.all()
+    serializer_class = MagazineSerializer
+
+
+    # def get(self,request):
+    #     magazines = Magazine.objects.all()
+    #     serializer = MagazineSerializer(magazines, many=True)
+    #     return Response(serializer.data)
